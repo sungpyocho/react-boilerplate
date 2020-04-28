e6f8c9e36dad43ad7e76033ee7cc1ca8e2d7e3c9
sungpyocho <korra0501@gmail.com>
2020-04-28 21:16:00 +0900

    Complete backend

--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const saltRounds = 10; // 해시 암호화를 할 때 필요한 salt의 길이
+
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -26,8 +30,68 @@ const userSchema = mongoose.Schema({
     }
 })
 
+// pre()를 처리하고 index.js의 save()를 실행함. next() -> save()
+userSchema.pre('save', function( next ) {
+    let user = this; // userSchema 가져오기
+
+    // 비밀번호가 변경될때만(isModified) 비밀번호를 해시 암호화!
+    if(user.isModified('password')) {
+        // 해시 암호화
+        bcrypt.genSalt(saltRounds, (err, salt) => {
+            if (err) return next(err);
+            
+            bcrypt.hash(user.password, salt, (err, encrypted) => {
+                if (err) return next(err);
+                user.password = encrypted;
+                next();
+            })
+        })
+    } else {
+        next();
+    }
+})
+
+// comparePassword라는 사용자 지정 메소드
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    // plainPassword == 해시된 비밀번호 ?
+    // cb는 콜백함수
+    bcrypt.compare(plainPassword, this.password, (err, isMatch) => {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    let user = this;
+
+    // jsonwebtoken을 이용해서 token을 생성
+    let token = jwt.sign(user._id.toHexString(), 'secret-Token');
+    user.token = token;
+    user.save((err, user) => {
+        if (err) return cb(err);
+        cb(null, user);
+    })
+}
+
+// static method는 User 모델에서 바로 쓸 수 있는 메소드
+userSchema.statics.findByToken = function(token, cb) {
+    let user = this;
+
+    // 토큰을 복호화한다.
+    jwt.verify(token, 'secret-Token',(err, decodedToken) => {
+        // 유저 아이디를 이용해서 유저를 찾은 다음에
+        // 클라이언트에서 가져온 토큰과 DB에 저장된 토큰이 일치하는지 확인
+        user.findOne({ "_id": decodedToken, "token": token}, (err, user) => {
+            if (err) return cb(err);
+            cb(null, user);
+        })
+
+    })
+}
+
+
 // Schema를 model로 감싸야 한다.
 const User = mongoose.model('User', userSchema);
 
-module.exports = { User }
+module.exports = { User };
 
