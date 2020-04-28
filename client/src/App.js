6ffccfc2a23b33534cc75b785e7034af9c66112b
sungpyocho <korra0501@gmail.com>
2020-04-28 23:25:42 +0900

    Add: Simple React&React Route code

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import './App.css';
+
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
+
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/register" component={RegisterPage} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
---
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RegisterPage() {
+    return (
+        <div>
+            등록좀해라
+        </div>
+    )
+}
+
+export default RegisterPage
